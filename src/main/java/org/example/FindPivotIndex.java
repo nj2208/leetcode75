57e277b4610cc8bffad493a34ecd0a1b9b357470
Nitin Jain <jnitin2015@gmail.com>
2023-01-23 23:39:32 +0530

    Added findpivot initial code

---
+++ src/main/java/org/example/FindPivotIndex.java
@@ -0,0 +1,71 @@
+package org.example;
+/**
+ * Given an array of integers nums, calculate the pivot index of this array.
+ *
+ * The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
+ *
+ * If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left. This also applies to the right edge of the array.
+ *
+ *
+ * Return the leftmost pivot index. If no such index exists, return -1.
+ *
+ * Input: nums = [2,1,-1]
+ * Output: 0
+ * Explanation:
+ * The pivot index is 0.
+ * Left sum = 0 (no elements to the left of index 0)
+ * Right sum = nums[1] + nums[2] = 1 + -1 = 0
+ *
+ */
+
+public class FindPivotIndex {
+    //{1,7,3,6,5,6};
+
+    //startIndex = 0
+
+    //endIndex = 1;
+    public int calculateSum(int[] arr , int startIndex , int endIndex){
+        int sum = 0;
+        if(startIndex == endIndex)
+            sum = arr[startIndex];
+
+        for (int pos = startIndex; pos < endIndex; pos++) {
+                sum = sum + arr[pos];
+        }
+
+        return sum;
+    }
+    public int pivotIndex(int[] nums) {
+        int pivot = -1; // Assume initialy no pivot exists until proven wrong
+        if(nums.length >= 1 && nums.length <= 1000 ) {
+                // Left Edge Case
+                if (0 == calculateSum(nums,1 , nums.length)) {
+                    System.out.println("left edge pivot");
+                    pivot = 0;
+                }
+                // Right Edge Case
+                else if (0 == calculateSum(nums,0 , nums.length -1 )) {
+                    System.out.println("right edge pivot");
+                    pivot = 0;
+                }
+                //  Not Left or right Edge Case
+                else {
+
+                    //{1,7,3,6,5,6};
+                    for (int index = 1; index < (nums.length - 1) ; index++) {
+                        System.out.println("pivot index  : " + index);
+                        int leftSum = calculateSum(nums,0 , index -1);
+                        System.out.println("leftSum : " + leftSum);
+                        int rightSum = calculateSum(nums,index + 1  , (nums.length));
+                        System.out.println("rightSum : " + rightSum);
+                        if(leftSum == rightSum) {
+                            System.out.println("middle pivot");
+                            pivot = index;
+                            break;
+                        }
+                    }
+                }
+        }
+        return pivot;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,18 +2,26 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello Leet Code 75 Days Plan !");
 
-        // Day 1 - Problem 1
-        RunningSumof1dArray array = new RunningSumof1dArray();
-        int[] arr = new int[10];
-        for(int i = 0; i < 10 ; i++) {
-            arr[i] = i;
-        }
-        int[] retVal = array.runningSum(arr);
-        for(int index = 0 ;index < retVal.length ; index++) {
-            System.out.println(retVal[index]);
-        }
+//        // Day 1 - Problem 1
+//        System.out.println("Day 1 Problem 1");
+//        RunningSumof1dArray array = new RunningSumof1dArray();
+//        int[] arr = new int[10];
+//        for(int i = 0; i < 10 ; i++) {
+//            arr[i] = i;
+//        }
+//        int[] retVal = array.runningSum(arr);
+//        for(int index = 0 ;index < retVal.length ; index++) {
+//            System.out.println(retVal[index]);
+//        }
+
+        // Day 1 - Problem 2
+        System.out.println("Day 1 Problem 2");
+        FindPivotIndex pivotIndx = new FindPivotIndex();
+        int[] arr1 = {1,7,3,6,5,6};
+        int pivot = pivotIndx.pivotIndex(arr1);
+        System.out.println(pivot);
 
     }
 }
