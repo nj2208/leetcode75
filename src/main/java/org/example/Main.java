406bbbda961b5a8f26f6bd466d6d19687f0a54c7
Nitin Jain <jnitin2015@gmail.com>
2023-01-23 20:43:54 +0530

    Adding running sum class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,11 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        RunningSumof1dArray array = new RunningSumof1dArray();
+        int[] arr = new int[10];
+        arr[0] = 2;
+        int[] retVal = array.runningSum(arr);
+        System.out.println(retVal[0]);
+
     }
 }
---
+++ src/main/java/org/example/RunningSumof1dArray.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class RunningSumof1dArray {
+    public int[] runningSum(int[] nums) {
+        System.out.println("Inside running Sum! ");
+        System.out.println(nums[0]);
+        return nums;
+    }
+
+}
