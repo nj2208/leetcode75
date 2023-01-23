4f00315a60183ac0d761552fdeedc77165f8240e
Nitin Jain <jnitin2015@gmail.com>
2023-01-23 21:31:45 +0530

    running sum of 1 day array problem

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,11 +3,17 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        // Day 1 - Problem 1
         RunningSumof1dArray array = new RunningSumof1dArray();
         int[] arr = new int[10];
-        arr[0] = 2;
+        for(int i = 0; i < 10 ; i++) {
+            arr[i] = i;
+        }
         int[] retVal = array.runningSum(arr);
-        System.out.println(retVal[0]);
+        for(int index = 0 ;index < retVal.length ; index++) {
+            System.out.println(retVal[index]);
+        }
 
     }
 }
--- src/main/java/org/example/RunningSumof1dArray.java
+++ src/main/java/org/example/RunningSumof1dArray.java
@@ -1,10 +1,27 @@
 package org.example;
 
+/*
+* Given an array nums. We define a running sum of an array as
+* runningSum[i] = sum(nums[0]…nums[i]).
+* Return the running sum of nums.
+* Example 1:
+* Input: nums = [1,2,3,4]
+* Output: [1,3,6,10]
+* Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4]
+* Constraints:
+1 <= nums.length <= 1000
+-10^6 <= nums[i] <= 10^6
+*/
 public class RunningSumof1dArray {
     public int[] runningSum(int[] nums) {
-        System.out.println("Inside running Sum! ");
-        System.out.println(nums[0]);
-        return nums;
+        int[] runningSum = new int[nums.length];
+        if(nums.length >= 1 && nums.length <= 1000 ) {
+            runningSum[0] = nums[0];
+            for(int index = 1 ; index < nums.length ; index++) {
+                runningSum[index] = runningSum[index-1]  + nums[index];
+            }
+        }
+        return runningSum;
     }
 
 }
