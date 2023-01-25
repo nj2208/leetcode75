c0a43257761ba30b1034153da7dcbca5a43c4bf8
Nitin Jain <jnitin2015@gmail.com>
2023-01-25 08:51:25 +0530

    Initial Code for isSubsequence

--- src/main/java/org/example/day2/Day2.java
+++ src/main/java/org/example/day2/Day2.java
@@ -1,12 +1,13 @@
 package org.example.day2;
 import org.example.day2.Isomorphic;
+import org.example.day2.Subsequence;
 
 
 public class Day2{
     public static void day2() {
         // Day 2 - Problem 1
-        System.out.println("Day 2 Problem 1");
-        Isomorphic str = new Isomorphic();
+//        System.out.println("Day 2 Problem 1");
+//        Isomorphic str = new Isomorphic();
 //        String str1 = "egg";
 //        String str2 = "add";
 //          String str1 = "badc";
@@ -15,9 +16,22 @@ public class Day2{
 //        String str2 = "bar";
 //        String str1 = "paper";
 //        String str2 = "title";
-        String str1 = "aaeaa";
-        String str2 = "uuxyy";
-        System.out.println("Is Isomorphic : " + str.isIsomorphic(str1,str2));
+//        String str1 = "aaeaa";
+//        String str2 = "uuxyy";
+//        System.out.println("Is Isomorphic : " + str.isIsomorphic(str1,str2));
+
+        System.out.println("Day 2 Problem 2");
+        Subsequence subsequence = new Subsequence();
+        String str1 = "badc";
+        String str2 = "baba";
+//        String str1 = "foo";
+//        String str2 = "bar";
+//        String str1 = "paper";
+//        String str2 = "title";
+//        String str1 = "aaeaa";
+//        String str2 = "uuxyy";
+        System.out.println("Is Subsequence : " + subsequence.isSubsequence(str1,str2));
+
     }
 
 
---
+++ src/main/java/org/example/day2/Subsequence.java
@@ -0,0 +1,11 @@
+package org.example.day2;
+
+public class Subsequence {
+    public boolean isSubsequence(String s, String t) {
+        boolean retVal = false;
+        if (s.equals(t)) {
+            retVal = true;
+        }
+        return retVal;
+    }
+}
