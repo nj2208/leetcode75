ae51b132045bfef68639c703f35392499f6e2d9f
Nitin Jain <jnitin2015@gmail.com>
2023-01-26 04:43:39 +0530

    Adding subsequence class

--- src/main/java/org/example/day2/Subsequence.java
+++ src/main/java/org/example/day2/Subsequence.java
@@ -1,10 +1,20 @@
 package org.example.day2;
 
+import static java.lang.Math.pow;
+//        String str1 = "foo";
+//        String str2 = "bar";
 public class Subsequence {
     public boolean isSubsequence(String s, String t) {
         boolean retVal = false;
+        s.toLowerCase();
+        t.toLowerCase();
         if (s.equals(t)) {
             retVal = true;
+        }
+        if ( (s.length() <= 100) && (( t.length() >=0)  || (s.length() >= 0))
+                && ( t.length() <= 10000 )  || (s.length() <= 10000) ) {
+
+
         }
         return retVal;
     }
