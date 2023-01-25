1caeb76d98978e26766de5ca632ea4e17b605eeb
Nitin Jain <jnitin2015@gmail.com>
2023-01-25 06:07:12 +0530

    isomorphic string check

--- src/main/java/org/example/day2/Day2.java
+++ src/main/java/org/example/day2/Day2.java
@@ -7,8 +7,16 @@ public class Day2{
         // Day 2 - Problem 1
         System.out.println("Day 2 Problem 1");
         Isomorphic str = new Isomorphic();
-        String str1 = "Hello";
-        String str2 = "World";
+//        String str1 = "egg";
+//        String str2 = "add";
+//          String str1 = "badc";
+//          String str2 = "baba";
+//        String str1 = "foo";
+//        String str2 = "bar";
+//        String str1 = "paper";
+//        String str2 = "title";
+        String str1 = "aaeaa";
+        String str2 = "uuxyy";
         System.out.println("Is Isomorphic : " + str.isIsomorphic(str1,str2));
     }
 
--- src/main/java/org/example/day2/Isomorphic.java
+++ src/main/java/org/example/day2/Isomorphic.java
@@ -1,7 +1,54 @@
 package org.example.day2;
+import java.util.HashMap;
+import java.util.Map;
 
+import static java.lang.Math.pow;
+// foo bar
+// egg add
+//
+//        String str1 = "paper";
+//        String str2 = "title";
 public class Isomorphic {
     public boolean isIsomorphic(String s, String t) {
-        return false;
+        boolean retVal = false;
+        if (s.equals(t)) {
+            retVal = true;
+        } else {
+            if ((s.length() == t.length()) && s.length() <= 5 * pow(10, 4) && s.length() >= 1) {
+                Map<Character, Character> map = new HashMap<>();
+                for (int index = 0; index < s.length(); index++) {
+                    if ((s.charAt(index) <= 127 && s.charAt(index) >= 1) &&
+                            (t.charAt(index) <= 127 && t.charAt(index) >= 1)) {
+                        if (map.isEmpty()) {
+                            // First
+                            map.put(s.charAt(index), t.charAt(index));
+
+                        } else {
+                            if (map.containsKey(s.charAt(index))) {
+                                if (map.get(s.charAt(index)) != t.charAt(index) ) {
+                                    retVal = false;
+                                    break;
+                                }
+                                // Repeat entry in left string . Check for this key if
+                                // corresponding same value exists in right string
+                                if (map.get(s.charAt(index)) == t.charAt(index) && (index == s.length() -1)) {
+                                    retVal = true;
+                                    break;
+                                }
+                            } else {
+                                    if (map.containsValue(t.charAt(index))) {
+                                        retVal = false;
+                                        break;
+                                    }
+
+                                map.put(s.charAt(index), t.charAt(index));
+                            }
+                        }
+                        retVal = true;
+                    }
+                }
+            }
+        }
+        return retVal;
     }
 }
